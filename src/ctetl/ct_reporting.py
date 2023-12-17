bbd43b1ed29ba84a07e35a0d5dd6a0b914dc4199
almlmla <58733471+almlmla@users.noreply.github.com>
2023-12-17 17:19:58 +0800

    refactored

--- src/ctetl/ct_reporting.py
+++ src/ctetl/ct_reporting.py
@@ -1,36 +1,24 @@
 # ct_reporting.py
 
-import io
-import json
-import os
-import sys
-
-from datetime import datetime, timedelta, timezone
-
-from minio import Minio
-from minio.commonconfig import Tags
-from minio.error import S3Error
+from datetime import datetime
 
 import pandas as pd
 
-import psycopg2
+from sqlalchemy import text
 
-import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from ctetl.ct_helpers import isoformat_to_seconds
 
-
-from sqlalchemy import create_engine, text
-from time import sleep
 
 def query_report_data_from_db(engine, start=96, end=72):
     """
+    Used by ct_score_report
+
     Changing timestamp data to local/UTC+8 for the reports.
     CrowdTangle timestamp data are always at UTC.
 
     """
 
-    if start < end:
+    if start <= end:
         print("Start must be greater than end.")
         sys.exit(1)
 
@@ -41,14 +29,15 @@ def query_report_data_from_db(engine, start=96, end=72):
             platform_id,
             post_message,
             post_url,
-            posting_date AT TIME ZONE 'UTC+8' AS local_posting_date,
-            as_of AT TIME ZONE 'UTC+8',
+            posting_date AT TIME ZONE 'UTC+8' AS sgt_posting_date,
+            as_of AT TIME ZONE 'UTC+8' as sgt_as_of,
             score,
             metric_timestep
     FROM accounts
     JOIN posts USING (account_id)
     JOIN post_metrics USING (platform_id)
-    WHERE posting_date BETWEEN CURRENT_TIMESTAMP - interval '{start} hours' AND CURRENT_TIMESTAMP - interval '{end} hours';
+    WHERE posting_date BETWEEN CURRENT_TIMESTAMP - interval '{start} hours' 
+    AND CURRENT_TIMESTAMP - interval '{end} hours';
     """
     )
     with engine.connect() as con:
@@ -59,12 +48,19 @@ def query_report_data_from_db(engine, start=96, end=72):
 
 def fill_missing_timesteps(df, max_timesteps=51):
     """
-    Timestep of 50 corresponds to an interval of between 2 days 18 hours and 3 days since the posting date.
-    Using 51 because, python.
+    Used by ct_score_report and generate_report.
+
+    Used to forward fill missing timestep values from last for every post.
+    Logically this works as any changes in values will have a timestep entry.
+
     """
 
-    # Initialize list to accumulate data
+    # Accumulate date in a list as it is cheaper than a DataFrame.
     result_data = []
+
+    # Forward fill missing timestep values from last.
+    # Logically this works as any changes in values
+    # will have a timestep entry
 
     for metric_timestep in range(max_timesteps):
         if metric_timestep in df["metric_timestep"].values:
@@ -77,23 +73,32 @@ def fill_missing_timesteps(df, max_timesteps=51):
                 last_valid_values["metric_timestep"] = metric_timestep
                 result_data.append(last_valid_values)
 
-    # Create a DataFrame from the list of dictionaries
-    result_df = pd.DataFrame(result_data)
-
-    return result_df
+    # finally create and return a DataFrame from the list of dictionaries
+    return pd.DataFrame(result_data)
 
 
 def generate_report(df):
+    """
+    Used by ct_score_report.
+
+    Generate a daily report of CrowdTangle scores from the supplied df.
+
+    Timestep of 50 corresponds to an interval of between 2 days 18 hours
+    and 3 days since the posting date.  Using 51 because of python counting.
+
+    """
     max_timesteps = 51
-    # Initialize list to accumulate data
+
+    # Accumulating data in a list as it is cheaper than a DataFrame
     report_data = []
 
     unique_platform_ids = df["platform_id"].unique()
 
     for platform_id in unique_platform_ids:
         post_df = df[df["platform_id"] == platform_id]
-        filled_df = fill_missing_timesteps(post_df, max_timesteps)
-        report_data.extend(filled_df.to_dict(orient="records"))
+        report_data.extend(
+            fill_missing_timesteps(post_df, max_timesteps).to_dict(orient="records")
+        )
 
     # Create a DataFrame from the list of dictionaries
     report_df = pd.DataFrame(report_data)
@@ -103,7 +108,15 @@ def generate_report(df):
 
 
 def save_report_to_csv(report_df):
+    """
+    Used by ct_score_report.
+
+    Save the report to a csv.  Include timestamp of report generation
+    in filename.
+
+    """
+
     now = datetime.now().replace(tzinfo=None)
-    as_of = now.isoformat(timespec="seconds")
-    filename = as_of.replace(":", "-") + "-ct_posts_report.csv"
+    as_of = isoformat_to_seconds(now)
+    filename = as_of.replace(":", "-") + "-ct_posts_score_report.csv"
     report_df.to_csv(filename, index=False, encoding="utf-8-sig")
